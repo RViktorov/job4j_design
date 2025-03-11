e14c18c09fca6748e2c66a334b08f747af781750
Roman Viktorov <rpviktorov@gmail.com>
2025-03-11 22:13:37 +0300

    fix1: 6. Читаем файл конфигурации [#858 #535522]

--- src/test/java/ru/job4j/io/ConfigTest.java
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -15,7 +15,7 @@ class ConfigTest {
     }
 
     @Test
-    void whenTemplateError() {
+    void whenStartWithEqualSign() {
         String path = "data/app1.properties";
         Config config = new Config(path);
         assertThatThrownBy(config::load)
@@ -29,5 +29,29 @@ class ConfigTest {
         config.load();
         assertThat(config.value("hibernate.dialect")).isEqualTo("org.hibernate.dialect.PostgreSQLDialect");
         assertThat(config.value("hibernate.connection.url")).isEqualTo("jdbc:postgresql=://127.0.0.1:5432/trackstudio");
+    }
+
+    @Test
+    void whenEndsWithEqualSign() {
+        String path = "data/app3.properties";
+        Config config = new Config(path);
+        assertThatThrownBy(config::load)
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void whenNoEqualSign() {
+        String path = "data/app4.properties";
+        Config config = new Config(path);
+        assertThatThrownBy(config::load)
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void whenOnlyEqualSign() {
+        String path = "data/app5.properties";
+        Config config = new Config(path);
+        assertThatThrownBy(config::load)
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
