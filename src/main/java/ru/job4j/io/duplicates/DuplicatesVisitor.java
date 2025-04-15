1209eeb2968fb3f5ed2737dccd898c3ea48cbcb5
Roman Viktorov <rpviktorov@gmail.com>
2025-04-15 23:42:02 +0300

    11. Поиск дубликатов [#315066]

---
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -0,0 +1,46 @@
+package ru.job4j.io.duplicates;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
+
+    private Map<FileProperty, ArrayList<Path>> table = new HashMap<>();
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attributes) throws IOException {
+        long size = attributes.size();
+        String fileName = file.getFileName().toString();
+        FileProperty fileProperty = new FileProperty(size, fileName);
+        if (table.containsKey((fileProperty))) {
+            ArrayList<Path> temp = table.get(fileProperty);
+            temp.add(file);
+            table.put(fileProperty, temp);
+        } else {
+            table.put(fileProperty, new ArrayList<>());
+            table.get(fileProperty).add(file);
+        }
+        return FileVisitResult.CONTINUE;
+    }
+
+    public void filePrint() {
+        for (Map.Entry<FileProperty, ArrayList<Path>> entry : table.entrySet()) {
+            ArrayList<Path> paths = entry.getValue();
+            if (paths.size() > 1) {
+                System.out.println(entry.getKey());
+                for (Path path : paths) {
+                    System.out.println(path);
+                }
+                System.out.println();
+            }
+        }
+    }
+}
