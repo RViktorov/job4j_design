b54e64068a0696b7e9a4fae5c7df1f2904fb9d4e
Roman Viktorov <rpviktorov@gmail.com>
2025-04-24 23:00:52 +0300

    13. Именованные аргументы [#295518]

---
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -0,0 +1,51 @@
+package ru.job4j.io;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArgsName {
+    private final Map<String, String> values = new HashMap<>();
+
+    public String get(String key) {
+        if (!values.containsKey(key)) {
+            throw new IllegalArgumentException("This key: '" + key + "' is missing");
+        }
+        return values.get(key);
+    }
+
+    private void parse(String[] args) {
+        for (String data : args) {
+            if (!data.contains("=")) {
+                throw new IllegalArgumentException("Error: This argument '" + data + "' does not contain an equal sign");
+            }
+            if (!data.startsWith("-")) {
+                throw new IllegalArgumentException("Error: This argument '" + data + "' does not start with a '-' character");
+            }
+            if (data.startsWith("-") && data.indexOf("=") == 1) {
+                throw new IllegalArgumentException("Error: This argument '" + data + "' does not contain a key");
+            }
+            if (data.indexOf("=") == data.length() - 1) {
+                throw new IllegalArgumentException("Error: This argument '" + data + "' does not contain a value");
+            }
+            values.put(data.substring(1, data.indexOf('=')), data.substring(data.indexOf('=') + 1));
+        }
+    }
+
+    public static ArgsName of(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Arguments not passed to program");
+        }
+        ArgsName names = new ArgsName();
+        names.parse(args);
+        return names;
+    }
+
+    public static void main(String[] args) {
+        ArgsName jvm = ArgsName.of(new String[]{"-Xmx=512", "-encoding=UTF-8"});
+        System.out.println(jvm.get("Xm1x"));
+
+        ArgsName zip = ArgsName.of(new String[]{"-out=project.zip", "-encoding=UTF-8"});
+        System.out.println(zip.get("out"));
+    }
+
+}
