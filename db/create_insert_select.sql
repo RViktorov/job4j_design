c6e5cb5ceaddb35e81cfe3d54c25529f56ae53bb
Roman Viktorov <rpviktorov@gmail.com>
2025-08-27 23:10:19 +0300

    fix1 0. Select [#6861]

--- db/create_insert_select.sql
+++ db/create_insert_select.sql
@@ -12,10 +12,12 @@ insert into fauna(name, avg_age, discovery_date)
 values ('Вороны', 10000, '1930-01-01');
 insert into fauna(name, avg_age, discovery_date)
 values ('Рыбы', 1000, '2000-01-01');
+insert into fauna(name, avg_age)
+values ('Птицы', 2000);
 
 --1) Извлечение данных, у которых имя name содержит подстроку fish
 select * FROM fauna
-where name LIKE '%fish';
+where name LIKE '%fish%';
 
 --2) Извлечение данных, у которых сред. продолжительность жизни находится в диапазоне 10 000 и 21 000
 select * FROM fauna
@@ -23,7 +25,7 @@ where avg_age BETWEEN 10000 AND 21000 ;
 
 --3) Извлечение данных, у которых дата открытия не известна (null)
 select * FROM fauna
-where discovery_date isnull ;
+where discovery_date is null ;
 --4) Извлечение данных видов, у которых дата открытия раньше 1950 года
 select * FROM fauna
 where discovery_date<'1950-01-01' ;
